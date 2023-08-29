aa9ef1fccc2074190b4d8c634fd1f71c250fa184
jagemdra <jagendrasrivastava007@gmail.com>
2023-08-29 11:58:42 +0530

    add Bycrypt gem and authenticate the password with the salt and authenticate method

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,12 +1,19 @@
 class User < ApplicationRecord
-    before_save { self.email = email.downcase}
-  has_many :articles
-  validates :username, presence:true,
-                       uniqueness: { case_sensitive: false },
-                       length: {minimum: 3, maximum: 20} 
-  VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-]+(\.[a-z\d\-]+)*\.[a-z]+\z/i         
-  validates :email, presence: true,
-                    uniqueness: { case_sensitive: false },
-                    length: {minimum: 5, maximum: 105},
-                    format: {with: VALID_EMAIL_REGEX} 
-end
+    before_save { self.email = email.downcase }
+    has_many :articles
+    validates :username, presence: true,
+                         uniqueness: { case_sensitive: false },
+                         length: { minimum: 3, maximum: 20 }
+    
+    unless defined?(VALID_EMAIL_REGEX)
+      VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-]+(\.[a-z\d\-]+)*\.[a-z]+\z/i
+    end
+    
+    validates :email, presence: true,
+                      uniqueness: { case_sensitive: false },
+                      length: { minimum: 5, maximum: 105 },
+                      format: { with: VALID_EMAIL_REGEX }
+    
+    has_secure_password
+  end
+  
