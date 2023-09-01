40ff95436dc90eaa105ccdc9b24284fcdbeebf03
jagemdra <jagendrasrivastava007@gmail.com>
2023-09-01 15:14:59 +0530

    add pagination on the Article, User profile and user article show page

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,7 +1,9 @@
 class ArticlesController < ApplicationController
   before_action :set_article, only: [:show, :edit, :update, :destroy]
+
+
   def index
-    @articles = Article.all
+    @articles = Article.paginate(page: params[:page], per_page: 3)
   end
 
   def show
